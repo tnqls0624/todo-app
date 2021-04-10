b998b65a8161867d2d7d2aadc97dbf302ba48af8
tnqls0624 <56984613+tnqls0624@users.noreply.github.com>
2021-04-10 20:28:11 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 3; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+  const nextId = useRef(4);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    setTodos((todos) => {
+      return todos.map((todo) => {
+        return todo.id === id ? { ...todo, checked: !todo.checked } : todo;
+      });
+    });
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => {
+      return todos.filter((todo) => todo.id !== id);
+    });
+  }, []);
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
+      <TodoList
+        todos={todos}
+        onRemove={onRemove}
+        onToggle={onToggle}
+      ></TodoList>
+    </TodoTemplate>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+import { List } from 'react-virtualized';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        ></TodoListItem>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={256}
+      height={257}
+      rowCount={todos.length}
+      rowHeight={26}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    ></List>
+  );
+};
+
+export default React.memo(TodoList);
